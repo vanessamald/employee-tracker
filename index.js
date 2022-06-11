f791f982ef6949519a7e38de0a6acad1f4d42133
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-06-11 16:24:09 -0500

    adding and testing inquirer.prompt

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cTable = require('console.table');
+
+function prompt() {
+    inquirer.prompt({
+        type: 'checkbox',
+        message: "What would you like to do?",
+        name: 'actions',
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees", 
+            "Add a department",
+            "Add a role",
+            "Update an employee role",
+            "EXIT"
+        ]
+    })
+}
+prompt();
+// view all departments
+//view all roles
+// view all employees
+// add a department
+// add a role
+// update an employee role
